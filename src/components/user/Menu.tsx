84f1fd2d90e8bdaf538d9b64a2bf53b044d4b218
Slim Hmidi <slim.hmidi1@gmail.com>
2020-03-02 00:30:16 +0100

    Add welcome message to user homepage

--- src/components/user/Menu.tsx
+++ src/components/user/Menu.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import AppBar from "../../common/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import './styles.css';
+import Grid from "@material-ui/core/Grid";
+
 
 
 const UserMenu = () => {
@@ -13,6 +16,11 @@ const UserMenu = () => {
     <div>
       <CssBaseline />
       <AppBar label="user" handleLogout={handleLogout} />
+      <Grid container alignItems="center" justify="center" style={{ height: "50vh" }}>
+        <div className="typewriter">
+          <h1>Welcome to user homepage</h1>
+        </div>
+      </Grid>
     </div>
   )
 }
